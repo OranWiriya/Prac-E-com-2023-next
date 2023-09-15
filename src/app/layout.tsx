37e5db9c372230fab48cc7964cf7b189bdff9d3e
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2023-09-16 01:43:46 +0700

    edited Navbar component name pascal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import './globals.css';
 import type { Metadata } from 'next';
 import theme from '@/config-mui/theme';
-import Navbar from '@/components/navbar/navbar';
+import Navbar from '@/components/Navbar';
 
 export const metadata: Metadata = {
   title: 'Fashion Website',
