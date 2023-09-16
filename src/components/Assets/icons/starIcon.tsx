c82f060909addf42e30b1614808f5d33117af562
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2023-09-17 01:32:41 +0700

    added StarIcon

---
+++ src/components/Assets/icons/starIcon.tsx
@@ -0,0 +1,22 @@
+import { SvgIcon } from '@mui/material';
+
+import React from 'react';
+
+function StarIcon() {
+  return (
+    <SvgIcon viewBox="0 0 24 24" className='relative top-1'>
+      <path
+        id="Vector"
+        d="M19.4672 23.3152L12.0002 17.8272L4.53323 23.3152L7.40023 14.4522L-0.0627747 8.99922H9.15123L12.0002 0.121216L14.8492 8.99922H24.0622L16.6002 14.4522L19.4672 23.3152Z"
+        fill="#ffffff"
+      />
+      <defs>
+        <clipPath id="clip0_1_120">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </SvgIcon>
+  );
+}
+
+export default StarIcon;
