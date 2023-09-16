3c995b3ec29a08b29bb7eaf95739651ea773c525
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2023-09-17 01:34:31 +0700

    added component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Display from "@/components/Display";
+import MainContent from "@/components/MainContent";
 
 export default function Home() {
   return (
     <>
       <Display />
+      <MainContent />
     </>
   );
 }
