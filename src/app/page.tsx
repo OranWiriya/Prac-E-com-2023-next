5f9a31fab64a1a3c141579db64321950d025e91b
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2023-09-24 02:12:26 +0700

    added comps. to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Display from "@/components/Display";
-import MainContent from "@/components/MainContent";
+import Banner from '@/components/Banner';
+import Display from '@/components/Display';
+import MainContent from '@/components/MainContent';
+import OurProduct from '@/components/OurProduct';
 
 export default function Home() {
   return (
     <>
       <Display />
       <MainContent />
+      <OurProduct />
+      <Banner />
     </>
   );
 }
