468ae40318497cad9e154ac98ae15c104bc7d791
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2023-09-24 02:12:14 +0700

    created Banner comp.

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+function Banner() {
+  return (
+    <Box>
+        <Typography>Exclusive offer</Typography>
+    </Box>
+  )
+}
+
+export default Banner
