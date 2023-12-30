b70e689fa334beec5063fb47975e06523cd4ad5a
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2023-12-30 14:43:32 +0700

    added svg dots

---
+++ src/components/Assets/icons/dotsIcon.tsx
@@ -0,0 +1,32 @@
+import { SvgIcon } from '@mui/material';
+
+import React from 'react';
+
+function DotsIcon() {
+  return (
+    <SvgIcon xmlns="http://www.w3.org/2000/svg" className='w-[156px] h-[110px]' viewBox="0 0 156 110" fill="none">
+  <circle cx="4" cy="4" r="4" fill="#859697"/>
+  <circle cx="4" cy="38" r="4" fill="#859697"/>
+  <circle cx="4" cy="72" r="4" fill="#859697"/>
+  <circle cx="4" cy="106" r="4" fill="#859697"/>
+  <circle cx="41" cy="4" r="4" fill="#859697"/>
+  <circle cx="41" cy="38" r="4" fill="#859697"/>
+  <circle cx="41" cy="72" r="4" fill="#859697"/>
+  <circle cx="41" cy="106" r="4" fill="#859697"/>
+  <circle cx="78" cy="4" r="4" fill="#859697"/>
+  <circle cx="78" cy="38" r="4" fill="#859697"/>
+  <circle cx="78" cy="72" r="4" fill="#859697"/>
+  <circle cx="78" cy="106" r="4" fill="#859697"/>
+  <circle cx="115" cy="4" r="4" fill="#859697"/>
+  <circle cx="115" cy="38" r="4" fill="#859697"/>
+  <circle cx="115" cy="72" r="4" fill="#859697"/>
+  <circle cx="115" cy="106" r="4" fill="#859697"/>
+  <circle cx="152" cy="4" r="4" fill="#859697"/>
+  <circle cx="152" cy="38" r="4" fill="#859697"/>
+  <circle cx="152" cy="72" r="4" fill="#859697"/>
+  <circle cx="152" cy="106" r="4" fill="#859697"/>
+</SvgIcon>
+  );
+}
+
+export default DotsIcon;
